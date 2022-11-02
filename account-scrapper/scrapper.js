205d9d6d53074bc4d7fd91b7011442b072851b99
trutzig89182 <adler.david@freenet.de>
2022-11-02 08:22:42 +0100

    Update scrapper.js

--- account-scrapper/scrapper.js
+++ account-scrapper/scrapper.js
@@ -1,33 +1,43 @@
-document.getElementById("account_url").addEventListener("click", getAcount);
+document.getElementById("get_data").addEventListener("click", getBio);
 
 
-function loadDoc() {
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function() {
-  document.getElementById("demo").innerHTML = this.responseText;
-  }
-xhttp.open("GET", "ajax_info.txt", true);
-xhttp.send();
-}
+// function loadDoc() {
+// const xhttp = new XMLHttpRequest();
+// xhttp.onload = function() {
+//   document.getElementById("demo").innerHTML = this.responseText;
+//   }
+// xhttp.open("GET", "ajax_info.txt", true);
+// xhttp.send();
+// }
 
+// var elements = document.getElementsByClassName("account__header__content emojify);
 
-function getAcount() {
-  var url = document.getElementById("account_url").value;
+//
+// function getAcount() {
+//   var url = document.getElementById("account_url").value;
+//
+//   console.log("url");
+//
+//   //$.ajax({ url: url, success: function(data) { alert(data); } });
+//
+//   htmlpage = data;
+//
+//   account = htmlpage.getElementByClassName("display-name__account").textContent
+//   console.log("account");
+//   display_name = htmlpage.getElementByClassName("display-name__html").textContent;
+//   console.log("display_name");
+//   account_url = url;
+//   console.log("account-url");
+//
+//   document.getElementById("result").innerHTML = (account + "," + display_name + "," + accounturl);
+//
+//
+// }
 
-  console.log("url");
-
-  //$.ajax({ url: url, success: function(data) { alert(data); } });
-
-  htmlpage = data;
-
-  account = htmlpage.getElementByClassName("display-name__account").textContent
-  console.log("account");
-  display_name = htmlpage.getElementByClassName("display-name__html").textContent;
-  console.log("display_name");
-  account_url = url;
-  console.log("account-url");
-
-  document.getElementById("result").innerHTML = (account + "," + display_name + "," + accounturl);
-
-
+function getBio() {
+  account_url = document.getElementById("account_url");
+  $.ajax({ url: "https://berlin.social/@AlexMitterle", success: function(data) {
+    var short_bio = data.getElementByClassName("account__header__content emojify").innerHTML;
+    document.getElementById("result").innerHTML = short_bio;
+  } });
 }
